051698e44d99c4974c0ddebc63752bd2b35a4ff9
siddht1 <dhar.75.ap@gmail.com>
2016-11-13 23:50:29 +0530

    Create OpertingSystemInfo.java

---
+++ new_comp/OpertingSystemInfo.java
@@ -0,0 +1,16 @@
+public class OpertingSystemInfo 
+{
+    public static void main(String[] args)
+    {
+        // The key for getting operating system name
+        String name = "os.name";        
+        // The key for getting operating system version
+        String version = "os.version";        
+        // The key for getting operating system architecture
+        String architecture = "os.arch";
+        
+        System.out.println("Name: " + System.getProperty(name));
+        System.out.println("Version: " + System.getProperty(version));
+        System.out.println("Architecture: " + System.getProperty(architecture));
+    }
+}
