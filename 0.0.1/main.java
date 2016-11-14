94db5bf4eb31a526fbbb7acb84b82aa36e718dd0
siddht1 <dhar.75.ap@gmail.com>
2016-11-14 15:13:57 +0530

    Update main.java

--- 0.0.1/main.java
+++ 0.0.1/main.java
@@ -1,1 +1,14 @@
-
+import java.io.*;
+public class main
+{
+public static void main(String args[]) throws IOException 
+{
+   
+     System.out.println("Enter your url or command ");
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String cmd_1=br.readLine();
+ String cmd="/usr/local/bin/youtube-dl "+cmd_1;
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd ).getInputStream()).useDelimiter("\\A");
+       System.out.println(s.hasNext() ? s.next() : "");
+    }
+}
